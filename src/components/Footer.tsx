2e9594589d0a83090e6c7bc4fc3d67e90e188faf
rahularya002 <ritiksaini258@gmail.com>
2024-11-13 11:16:40 +0530

    added support page link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,6 +5,9 @@ export default function Footer() {
     <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
       <p className="text-xs text-muted-foreground">&copy; 2024 MD INFOSYSTEM. All rights reserved.</p>
       <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+        <Link href="/Support" className="text-xs hover:underline underline-offset-4" prefetch={false}>
+          Contact us
+        </Link>
         <Link href="/Support/Terms" className="text-xs hover:underline underline-offset-4" prefetch={false}>
           Terms of Service
         </Link>
